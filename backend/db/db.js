d9d444a23513e306e716ab586c991b98e7594ab0
Kuruin <manavjit9@gmail.com>
2025-07-25 11:38:14 +0530

    Backend: Add connectDB error handling

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://ce23manavjitsinghtanda:HXtzXfdjNhuB8mvL@cluster0.rqfcx.mongodb.net/public_todo_app?retryWrites=true&w=majority&appName=Cluster0');
-    console.log('Mongooese database connected');
+    try {
+        await mongoose.connect('mongodb+srv://ce23manavjitsinghtanda:HXtzXfdjNhuB8mvL@cluster0.rqfcx.mongodb.net/public_todo_app?retryWrites=true&w=majority&appName=Cluster0');
+        console.log('Mongooese database connected');
+    } catch (err) {
+        console.error('MongoDB connection failed:', err);
+        process.exit(1);
+    }
 }
 const todoSchema = new mongoose.Schema({
     name: String,
